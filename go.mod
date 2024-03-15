e2e93daa59533b22bcc469c0db2f3879580cd6cb
David Sabatie <david.sabatie@agicap.com>
2024-03-15 12:22:12 +0100

    feat: initial gptscript integration

--- go.mod
+++ go.mod
@@ -1,14 +1,50 @@
 module github.com/golgoth31/gitcomm
 
-go 1.21.1
+go 1.22.0
+
+toolchain go1.22.1
 
 require (
+	github.com/gptscript-ai/gptscript v0.2.1
 	github.com/jawher/mow.cli v1.2.0
 	github.com/karantin2020/promptui v0.1.0
+	github.com/rs/zerolog v1.32.0
 )
 
 require (
+	github.com/BurntSushi/locker v0.0.0-20171006230638-a6e239ea1c69 // indirect
+	github.com/adrg/xdg v0.4.0 // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/bodgit/plumbing v1.2.0 // indirect
+	github.com/bodgit/sevenzip v1.3.0 // indirect
+	github.com/bodgit/windows v1.0.0 // indirect
 	github.com/chzyer/test v1.0.0 // indirect
+	github.com/connesc/cipherio v0.2.1 // indirect
+	github.com/dsnet/compress v0.0.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jaytaylor/html2text v0.0.0-20230321000545-74c2419ad056 // indirect
 	github.com/karantin2020/readline v0.0.0-20190318024627-af343dd535e9 // indirect
+	github.com/klauspost/compress v1.16.5 // indirect
+	github.com/klauspost/pgzip v1.2.5 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.10 // indirect
+	github.com/mholt/archiver/v4 v4.0.0-alpha.8 // indirect
+	github.com/nwaples/rardecode/v2 v2.0.0-beta.2 // indirect
+	github.com/olekukonko/tablewriter v0.0.6-0.20230925090304-df64c4bbad77 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/rivo/uniseg v0.1.0 // indirect
+	github.com/sashabaranov/go-openai v1.20.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
+	github.com/therootcompany/xz v1.0.1 // indirect
+	github.com/ulikunitz/xz v0.5.10 // indirect
+	go4.org v0.0.0-20200411211856-f5505b9728dd // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
---
+++ pkg/gptscript/gptscript.go
@@ -0,0 +1,131 @@
+package gptscript
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/gptscript-ai/gptscript/pkg/cache"
+	"github.com/gptscript-ai/gptscript/pkg/engine"
+	"github.com/gptscript-ai/gptscript/pkg/llm"
+	"github.com/gptscript-ai/gptscript/pkg/loader"
+	"github.com/gptscript-ai/gptscript/pkg/monitor"
+	"github.com/gptscript-ai/gptscript/pkg/mvl"
+	"github.com/gptscript-ai/gptscript/pkg/openai"
+	"github.com/gptscript-ai/gptscript/pkg/repos/runtimes"
+	"github.com/gptscript-ai/gptscript/pkg/runner"
+	"github.com/gptscript-ai/gptscript/pkg/types"
+
+	"github.com/rs/zerolog/log"
+)
+
+type (
+	DisplayOptions monitor.Options
+	CacheOptions   cache.Options
+	OpenAIOptions  openai.Options
+)
+
+type GPTScript struct {
+	CacheOptions
+	OpenAIOptions
+	DisplayOptions
+
+	_client llm.Client `usage:"-"`
+}
+
+const (
+	gptProgram = `tools: gitstatus, sys.abort
+
+Create well formed git commit message based of off the currently staged file
+contents. The message should convey why something was changed and not what
+changed. Use the well known format that has the prefix fix, etc but not chore.
+
+Do not use markdown format for the output.
+
+If there are no changes abort.
+
+---
+name: gitstatus
+
+#!/bin/sh
+
+git diff --staged`
+)
+
+func (r *GPTScript) Run(ctx context.Context, args []string) error {
+	log.Debug().Msg("Starting GPTScript")
+	defer engine.CloseDaemons()
+
+	var (
+		prg types.Program
+		err error
+	)
+
+	mvl.SetSimpleFormat()
+	mvl.SetError()
+
+	log.Debug().Msg("Loading program")
+	prg, err = loader.ProgramFromSource(ctx, gptProgram, "")
+	if err != nil {
+		return err
+	}
+
+	log.Debug().Msg("Getting client")
+	client, err := r.getClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Creating runner")
+	r.DisplayOptions.DisplayProgress = false
+	runner, err := runner.New(client, runner.Options{
+		MonitorFactory: monitor.NewConsole(monitor.Options(r.DisplayOptions), monitor.Options{
+			DisplayProgress: false,
+		}),
+		RuntimeManager: runtimes.Default(cache.Complete(cache.Options(r.CacheOptions)).CacheDir),
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Running program")
+	s, err := runner.Run(ctx, prg, os.Environ(), "")
+	if err != nil {
+		return err
+	}
+
+	fmt.Print(s)
+	if !strings.HasSuffix(s, "\n") {
+		fmt.Println()
+	}
+
+	return nil
+}
+
+func (r *GPTScript) getClient(ctx context.Context) (llm.Client, error) {
+	if r._client != nil {
+		return r._client, nil
+	}
+
+	cacheClient, err := cache.New(cache.Options(r.CacheOptions))
+	if err != nil {
+		return nil, err
+	}
+
+	oaClient, err := openai.NewClient(openai.Options(r.OpenAIOptions), openai.Options{
+		Cache: cacheClient,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	registry := llm.NewRegistry()
+
+	if err := registry.AddClient(ctx, oaClient); err != nil {
+		return nil, err
+	}
+
+	r._client = registry
+	return r._client, nil
+}
